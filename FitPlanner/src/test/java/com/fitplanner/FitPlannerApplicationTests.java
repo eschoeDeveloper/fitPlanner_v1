78b7be3093ef7a0965a710142a508608d69eec3f
uschoedev <dev_uschoe@codebplat.co.kr>
2022-10-15 19:49:51 +0900

    [FIRST] 프로젝트 최초 푸시

---
+++ FitPlanner/src/test/java/com/fitplanner/FitPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

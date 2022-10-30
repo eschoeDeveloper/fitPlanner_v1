3d3f11151d1a193d282f4bd45f2264324b2d0e64
uschoedev <dev_uschoe@codebplat.co.kr>
2022-10-31 00:06:59 +0900

    [ADD] 회원가입 완료 및 ssoLogin, vue cookie 및 security 적용 완료

---
+++ FitPlanner/src/main/java/com/fitplanner/core/config/AwsConfig.java
@@ -0,0 +1,46 @@
+package com.fitplanner.core.config;
+
+//import com.amazonaws.auth.AWSStaticCredentialsProvider;
+//import com.amazonaws.auth.BasicAWSCredentials;
+//import com.amazonaws.services.s3.AmazonS3;
+//import com.amazonaws.services.s3.AmazonS3Client;
+//import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.env.Environment;
+
+/**
+ * package     : com.fitplanner.core.config
+ * file        : AwsConfig
+ * author      : choeuiseung
+ * date        : 2022/10/29
+ * description
+ * =======================================================
+ * DATE                   AUTHOR                    NOTE
+ * =======================================================
+ * 2022/10/29                choeuiseung                 최초 생성
+ * =======================================================
+ */
+//@Configuration
+public class AwsConfig {
+
+//    @Autowired
+//    private Environment env;
+//
+//    @Bean("awsS3Client")
+//    public AmazonS3Client amazonS3Client() {
+//
+//        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials("sss", "sss");
+//        AWSStaticCredentialsProvider provider = new AWSStaticCredentialsProvider(basicAWSCredentials);
+//
+//        AmazonS3Client amazonS3Client = (AmazonS3Client) AmazonS3ClientBuilder.standard()
+//                                                .withRegion("ss")
+//                                                .withCredentials(provider)
+//                                                .build();
+//
+//        return amazonS3Client;
+//
+//    }
+
+}
---
+++ FitPlanner/src/main/java/com/fitplanner/domain/s3/service/S3ClientService.java
@@ -0,0 +1,35 @@
+package com.fitplanner.domain.s3.service;
+
+//import com.amazonaws.services.s3.AmazonS3Client;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Service;
+
+/**
+ * package     : com.fitplanner.domain.s3.service
+ * file        : S3ClientService
+ * author      : choeuiseung
+ * date        : 2022/10/29
+ * description
+ * =======================================================
+ * DATE                   AUTHOR                    NOTE
+ * =======================================================
+ * 2022/10/29                choeuiseung                 최초 생성
+ * =======================================================
+ */
+//@Service
+//@RequiredArgsConstructor
+public class S3ClientService {
+
+//    @Autowired
+//    private AmazonS3Client amazonS3Client;
+//
+//    @Value("${cloud.aws.s3.bucket}")
+//    private String s3Bucket;
+//
+//    public String getFilePath(String path) {
+//        return amazonS3Client.getUrl(s3Bucket, path).toString();
+//    }
+
+}
